0735fa092b9af4fe016dec09e839caf4d8209869
dijoncavalcante <dijoncavalcante@gmail.com>
2020-10-03 17:52:46 -0400

    Updated Fruitservice.java

--- app/src/main/java/interfaces/FruitService.java
+++ app/src/main/java/interfaces/FruitService.java
@@ -1,11 +1,9 @@
 package interfaces;
 
 
-import model.Fruit;
 import model.RequisicaoObj;
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 //.baseUrl("http://tropicalfruitandveg.com/")
@@ -13,15 +11,11 @@ public interface FruitService {
     @GET("api/tfvjsonapi.php?search=all")
     Call<RequisicaoObj> getAllFruits();
 
-    @GET("api/tfvxmlapi.php?search=banana")
-    Call<Fruit> getFruit();
+    @GET("api/tfvxmlapi.php")
+    Call<RequisicaoObj> getSearch(@Query("search") String itemSearch);
 
-//    @GET("http://api.tropicalfruitandveg.com/tfvjsonapi.php?tfvitem={fruit}")
-//    Call<Fruit> getFruitDetail(@Path("fruit") String fruit);
-//http://tropicalfruitandveg.com/api/tfvjsonapi.php?tfvitem=banana
-//    @GET("api/tfvjsonapi.php?tfvitem={fruit}")
     @GET("api/tfvjsonapi.php")
-    Call<RequisicaoObj> getFruitDetail(@Query("tfvitem") String fruit);
+    Call<RequisicaoObj> getFruitDetail(@Query("tfvitem") String item);
 
 
 }
