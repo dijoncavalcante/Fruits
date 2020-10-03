bd2a4af9d17fc7d96b51575867cf7aa376e422c6
dijoncavalcante <dijoncavalcante@gmail.com>
2020-10-03 14:01:43 -0400

    Add Detail activity class

---
+++ app/src/main/java/com/dijon/fruit/Detail_Activity.java
@@ -0,0 +1,14 @@
+package com.dijon.fruit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Detail_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_);
+    }
+}
